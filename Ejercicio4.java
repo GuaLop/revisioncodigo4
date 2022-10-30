0f385acf7e7554dfe5db32df4b8725393424c567
GuaLop <lvg051091@gmail.com>
2022-10-30 17:54:41 -0600

    Fix: corrección del código

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -2,38 +2,42 @@ package com.generation;
 
 public class Codigo4 {
 
-    Scanner s = new Scanner();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
+    //Se agrega el metodo public Static
+    public static void main(String[] args) {
 
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
-      }
-      System.out.println("Gana el jugador " + g);
+        //se agrega System.in
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Turno del jugador 1 introduzca piedra papel o tijeras: ");
+        String j1 = s.nextLine();
+
+        System.out.print("Turno del jugador 2 introduzca piedra papel o tijeras: ");
+        String j2 = s.nextLine();
+
+        //se agrrega .equals para validar los strings.
+        if (j1.equals(j2)) {
+            System.out.println("Empate");
+        } else {
+            int ganador = 2;
+
+            switch(j1) {
+                case "piedra":
+                    if (j2.equals("tijeras")) {
+                        ganador = 1;
+                    }
+                    break;
+
+                case "papel":
+                    if (j2.equals("piedra")) {
+                        ganador = 1;
+                    }
+                    break;
+                case "tijeras":
+                    if (j2.equals("papel")) {
+                        ganador = 1;
+                    }
+            }
+            System.out.println("Gana el jugador " + ganador);
+        }
     }
-  
-  
 }
